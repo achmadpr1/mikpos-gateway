99590229ef5a72b81cb895c1ac41468415094be7
achmadpr1 <achmadpr11@gmail.com>
2024-09-01 23:28:57 +0700

    first commit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -32,9 +32,9 @@ class Database extends \CodeIgniter\Database\Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => 'root',
-		'password' => '',
-		'database' => 'mikpos',
+		'username' => 'admin',
+		'password' => 'achmadpr',
+		'database' => 'mikposdb',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
